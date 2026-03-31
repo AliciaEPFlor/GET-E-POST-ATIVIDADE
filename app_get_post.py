a6b956637fbc16c3184aeae4724ae73f50b792d0
AliciaEPFlor <aliciaelizabethpilarflor@gmail.com>
2026-03-31 08:21:32 -0300

    Alterações nos codigos e correção

--- app_get_post.py
+++ app_get_post.py
@@ -1,29 +1,22 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer,BaseHTTPRequestHandler
 
 class Servidor(BaseHTTPRequestHandler):
-    # Método GET (já usado antes)
+
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Servidor funcionando com GET")
+        self.wfile.write(b"Servidor WEB funcionando! Bem vindo!")
+    
+    def do_POST(self):
+        tamanho = int(self.headers['Content-Length'])
+        dados = self.rfile.read(tamanho)
+        print("Dados recebidos!", dados.decode())
 
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"POST recebido!")
+        self.wfile.write(b"User registrado")
 
-# Método POST (novo)
-def do_POST(self):
+HTTPServer (("0.0.0.0", 8000), Servidor).serve_forever() 
 
-   # pega o tamanho dos dados enviados na requisição
-   tamanho = int(self.headers['Content-Length'])
-
-   # lê os dados enviados
-   dados = self.rfile.read(tamanho)
-
-   # mostra os dados recebidos no terminal
-   print("Dados recebidos:", dados.decode())
-
-   # envia resposta para o cliente
-   self.send_response(200)
-   self.end_headers()
-   self.wfile.write(b"POST recebido")
-
-HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
-
+#Esse código cria um servidor local para rodar HTTP, GET E POST
